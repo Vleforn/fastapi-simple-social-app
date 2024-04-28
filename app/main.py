ea58606f674bc30aed0dc1909228c8be79785ed8
vleforn <vleforn@gmail.com>
2024-04-29 01:07:08 +0500

    feat: CORS middleware

--- app/main.py
+++ app/main.py
@@ -2,8 +2,17 @@ from fastapi import FastAPI, status
 from . import models
 from .database import engine
 from .routers import post, user, auth, vote
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # disable sqlalchemy table creation
 # models.Base.metadata.create_all(bind=engine)
